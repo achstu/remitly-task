da126fb09928ca3bdb417948910d50d0ed74915f
Michał Stachurski <root.stachu@gmail.com>
2025-04-23 19:51:41 +0200

    Script for creating db

---
+++ create_db.py
@@ -0,0 +1,17 @@
+# script for scrapping spreadsheet (csv) to sqlite3 database
+import csv
+from model import Bank
+
+
+db = Bank._meta.database
+db.connect()
+db.create_tables([Bank])
+
+with open("swift_codes.csv", newline="") as file:
+    reader = csv.reader(file)
+    columns = list(map(lambda col: col.replace(" ", "_").lower(), next(reader)))
+
+    for row in reader:
+        Bank.create(**dict(zip(columns, row)))
+
+db.close()
