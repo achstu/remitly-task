c2447af3585f6f3420f0dc4657356de892f7aadf
Michał Stachurski <root.stachu@gmail.com>
2025-04-23 20:39:50 +0200

    Updating model

--- model.py
+++ model.py
@@ -1,15 +1,15 @@
-import peewee
+from peewee import Model, TextField, FixedCharField, SqliteDatabase
 
-class Bank(peewee.Model):
-    country_iso2_code = peewee.TextField()
-    swift_code = peewee.FixedCharField(primary_key=True, max_length=16)
-    code_type = peewee.TextField()
-    name = peewee.TextField()
-    address = peewee.TextField(null=True)
-    town_name = peewee.TextField()
-    country_name = peewee.TextField()
-    time_zone = peewee.TextField()
+class Bank(Model):
+    country_iso2_code = TextField()
+    swift_code = FixedCharField(primary_key=True, max_length=16)
+    code_type = TextField()
+    name = TextField()
+    address = TextField(null=True)
+    town_name = TextField()
+    country_name = TextField()
+    time_zone = TextField()
 
     class Meta:
-        database = peewee.SqliteDatabase('banks.db')
+        database = SqliteDatabase('banks.db')
         table_name = 'banks'
